3e7ada83d405de803a27d76c119b37819d023cb0
Jiang Qiqi <woshiqiqiye@gmail.com>
2016-06-01 08:36:35 +0800

    add some additional fields

--- lianjia/items.py
+++ lianjia/items.py
@@ -7,3 +7,6 @@ class lianjiaItem(scrapy.Item):
     price_of_house = scrapy.Field()
     area_of_house = scrapy.Field()
     time_of_construction = scrapy.Field() #该字段将会分为：1、房屋所属楼层 2、 建设时间
+    county = scrapy.Field()
+    city = scrapy.Field()
+    date = scrapy.Field()
--- lianjia/spiders/lianjia_spider.py
+++ lianjia/spiders/lianjia_spider.py
@@ -1,34 +1,43 @@
 import scrapy
 from lianjia.items import lianjiaItem
+from datetime import datetime
 
 class lianjiaSpider(scrapy.Spider):
-    name = 'lianjia'
-    allowed_domains = ["lianjia.com"]
-    city_and_page = {
+    def __init__(self, args):
+        "docstring"
+
+        name = 'lianjia'
+        allowed_domains = ["lianjia.com"]
+        city_and_page = {
             'dongcheng': 99, 'cicheng': 100, 'chaoyang': 100, 'haidian': 100,
             'fengtai': 100, 'shijingshan': 96, 'tongzhou': 93, 'changping': 100,
             'daxing': 100, 'yizhuangkaifaqu': 52, 'shunyi': 100, 'fangshan': 100, 
             'mentougou': 49, 'pinggu': 0, 'huairou': 1, 'miyun': 1, 'yanqing': 1,
             'yanjiao': 100}
 
-    house_type = "ershoufang"
-    county = "chaoyang"
-    city = "bj"
-    
-    base_url = "http://{ct}.lianjia.com/{t}/{c}/pg%{n}"
+        house_type = "ershoufang"
+        county = ["haidian", "chaoyang"]
+        city = "bj"
 
-    start_urls = []
-    for i in xrange(1, 100):
-        start_urls.append(base_url.format(ct=city, c=county, t=house_type, n=i+1))
+        base_url = "http://{ct}.lianjia.com/{t}/{c}/pg%{n}"
 
-    keyfunMap = {"name_of_community": "div[1]/div[1]/a/span/text()" ,
-                 'layout_of_house': "div[1]/div[1]/span[1]/span/text()",
-                 'price_of_house':  "div[2]/div[1]/span/text()",
-                 'area_of_house': "div[1]/div[1]/span[2]/text()",
-                 'time_of_construction': "div[1]/div[2]/div/text()"}
+        start_urls = []
+        for i in xrange(1, 100):
+            start_urls.append(base_url.format(ct=city, c=county, t=house_type, n=i+1))
+
+        keyfunMap = {"name_of_community": "div[1]/div[1]/a/span/text()" ,
+                     'layout_of_house': "div[1]/div[1]/span[1]/span/text()",
+                     'price_of_house':  "div[2]/div[1]/span/text()",
+                     'area_of_house': "div[1]/div[1]/span[2]/text()",
+                     'time_of_construction': "div[1]/div[2]/div/text()"}
+
     def parse(self, response):
         for sel in response.xpath("//div[@class='info-panel']"):
             lianjia = lianjiaItem()
             for k,v in self.keyfunMap.items():
                 lianjia[k] = sel.xpath(v).extract()[0].strip()
+
+            lianjia["date"] = datetime.now().strftime('%Y-%m-%d')
+            lianjia["city"] = self.city
+            lianjia["county"] = self.county
             yield lianjia
